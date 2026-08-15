e05f1119ec6e4f2fb0c1d2e2cca7228d57c7d0cc
christworldorder <admin@christworldorder.com>
2026-08-14 23:07:32 -0500

    Add Meta Pixel 946085798523529 to site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,6 +34,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Meta Pixel */}
+        <script dangerouslySetInnerHTML={{ __html: `
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window,document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init','946085798523529');
+          fbq('track','PageView');
+        `}} />
+        <noscript dangerouslySetInnerHTML={{ __html: `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=946085798523529&ev=PageView&noscript=1"/>` }} />
+      </head>
       <body className="bg-gray-50 min-h-screen">
         <CartProvider>
           {/* Promo announcement bar */}
